a6ef854d26b71e6fb1d24439db0389980a6315da
Xiaodan Xu <arielinseu@gmail.com>
2022-02-03 16:27:48 -0800

    add NPMRDS for austin and process SF network

--- Users/Xiaodan/SF2018/BEAM_network_to_GIS.R
+++ Users/Xiaodan/SF2018/BEAM_network_to_GIS.R
@@ -1,7 +1,7 @@
 library(sf)
 library(dplyr)
 library(data.table)
-file_link <- '/Volumes/GoogleDrive/My Drive/BEAM-CORE/BEAM Validation/sample output/AUS2018/'
+file_link <- '/Volumes/GoogleDrive/My Drive/BEAM-CORE/BEAM Validation/sample output/SFB2010/'
 beam_network <-read.csv(paste0(file_link, 'network.csv.gz'))
 beam_network_onode_sf = st_as_sf(beam_network, coords = c('fromLocationX', 'fromLocationY'), 
                                 crs = 26910, agr = "constant")
@@ -25,24 +25,27 @@ line <- pair %>%
 plot(st_geometry(line))
 
 beam_network_out <- st_transform(line, crs = 4326)
-st_write(beam_network_out, paste0(file_link, "beam_network_out.geojson"))
+st_write(beam_network_out, paste0(file_link, "beam_network_out.geojson"), append=FALSE)
 
 
 #### clean up beam network and assign county ####
 
 beam_network_out <- st_read(paste0(file_link, "beam_network_out.geojson"))
-austin_boundary <- st_read('/Volumes/GoogleDrive/My Drive/BEAM-CORE/BEAM Validation/data for validation/Austin/austin_counties.shp')
-austin_boundary <- st_transform(austin_boundary, 4326)
+sf_boundary <- st_read('/Volumes/GoogleDrive/My Drive/BEAM-CORE/BEAM Validation/data for validation/SF/SF_counties.geojson')
+sf_boundary <- st_transform(sf_boundary, 4326)
 
-beam_network_splits <- st_intersection(beam_network_out, austin_boundary)
-beam_network_splits$length <- st_length(beam_network_splits)
-plot(st_geometry(austin_boundary), border = 'blue')
+beam_network_splits <- st_intersection(beam_network_out, sf_boundary)
+# beam_network_splits$length <- st_length(beam_network_splits)
+plot(st_geometry(sf_boundary), border = 'blue')
 plot(st_geometry(beam_network_splits), add = TRUE)
 
 beam_network_splits <- beam_network_splits %>%
-  select(linkId, linkLength, length, linkFreeSpeed, linkCapacity, numberOfLanes, linkModes, attributeOrigId, attributeOrigType,
-         fromNodeId, toNodeId, name, txdot_abbr, fips_code)
-plot(beam_network_splits[, 'name'], axes = TRUE, key.pos = 4, key.width = lcm(4.5))
+  select(linkId, linkLength, linkFreeSpeed, linkCapacity, numberOfLanes, linkModes, 
+         attributeOrigId, attributeOrigType,
+         fromNodeId, toNodeId, GEOID, NAME, NAMELSAD, CLASSFP)
+
+beam_network_splits <- beam_network_splits %>% filter(linkLength > 0.001)
+plot(beam_network_splits[, 'NAME'], axes = TRUE, key.pos = 4, key.width = lcm(4.5))
 
 beam_network_splits_df <- beam_network_splits %>% st_drop_geometry()
 write.csv(beam_network_splits_df, paste0(file_link, "beam_network_by_county.csv"))
