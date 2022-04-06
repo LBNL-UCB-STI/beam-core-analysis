f0f39f2d49a69e191ad21180edc3c67d2a709315
Zach Needell <zaneedell@lbl.gov>
2022-04-05 17:16:20 -0700

    set up to run all directories

--- Users/Zach/produce-act-outputs-v2.py
+++ Users/Zach/produce-act-outputs-v2.py
@@ -232,13 +232,32 @@ if __name__ == '__main__':
 
     conn = boto.s3.connect_to_region('us-east-2')
     bucket = conn.get_bucket('beam-outputs')
-    runName = 'sfbay-transit_frequencies_0.5-20220228'
-    folders = bucket.list("pilates-outputs/" + runName + "/beam/", "/")
-    allBeamOutputs = [folder.name for folder in folders]
-
-    inDirectory = allBeamOutputs[-1]
-    popDirectory = inDirectory.replace("/beam/", "/activitysim/")
-    outDirectory = 'out/' + runName
-    collectAllData(inDirectory, outDirectory, popDirectory)
+    allRuns = ["sfbay-2018-base-20220327",
+               "sfbay-RH_fleetsz_0.25-20220329",
+               "sfbay-RH_fleetsz_0.5-20220329",
+               "sfbay-RH_fleetsz_0.67-20220328-failed",
+               "sfbay-RH_fleetsz_1.5-20220328",
+               "sfbay-RH_fleetsz_1.75-20220329",
+               "sfbay-transit_capacity_0.5-20220329",
+               "sfbay-transit_frequencies_0.5-20220228",
+               "sfbay-transit_frequencies_1.5-20220228",
+               "sfbay-transit_frequencies_2.0-20220229",
+               "sfbay-transit_speed_1.5-20220228"]
+    for runName in allRuns:
+        folders = bucket.list("pilates-outputs/" + runName + "/beam/", "/")
+        allBeamOutputs = [folder.name for folder in folders if ('year' in folder.name)]
+        yearToMaxIter = dict()
+        for dir in allBeamOutputs:
+            yr = dir.split('-')[-3]
+            it = int(dir.split('-')[-1][:-1])
+            if yr in yearToMaxIter:
+                if it > yearToMaxIter[yr][0]:
+                    yearToMaxIter[yr] = (it, dir)
+            else:
+                yearToMaxIter[yr] = (it, dir)
+        for yr, (_, inDirectory) in yearToMaxIter.items():
+            popDirectory = inDirectory.replace("/beam/", "/activitysim/")
+            outDirectory = "s3://beam-outputs/" + "-".join(inDirectory.split("-")[:-2]) + "-final/"
+            collectAllData(inDirectory, outDirectory, popDirectory)
 
     print('done')
