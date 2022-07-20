2176c1677d9afc1ad2a6dcb3de13e812a01fd0b8
annitodd <97706543+annitodd@users.noreply.github.com>
2022-07-19 17:10:12 -0700

    update

--- Users/Annika/2022_06_14_global_file_directories.R
+++ Users/Annika/2022_06_14_global_file_directories.R
@@ -8,3 +8,8 @@ print("data_dir_on_this_machine")
 print(data_dir_on_this_machine)
 
 
+output_dir_on_this_machine <-  "C:/Users/annik/tmpOnATBcomputer/output/"
+print("output_dir_on_this_machine")
+print(output_dir_on_this_machine)
+
+
