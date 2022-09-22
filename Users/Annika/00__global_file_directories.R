31192df70911fecac92399f2380cad6e7ddcb0b5
annitodd <97706543+annitodd@users.noreply.github.com>
2022-09-22 12:21:52 -0700

    2022 09 22

---
+++ Users/Annika/00__global_file_directories.R
@@ -0,0 +1,15 @@
+
+
+# Define global variables for path names on this machine
+# save in same path as the Rmd Code
+
+data_dir_on_this_machine <-  "C:/Users/annik/tmpOnATBcomputer/data/"
+print("data_dir_on_this_machine")
+print(data_dir_on_this_machine)
+
+
+output_dir_on_this_machine <-  "output/"
+print("output_dir_on_this_machine")
+print(output_dir_on_this_machine)
+
+
